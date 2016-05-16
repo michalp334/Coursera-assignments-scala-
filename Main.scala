9b895b4f6df428d06e209f5c92bf867536da0d24
michalp334 <michal.porwisz@gmail.com>
2016-05-16 05:22:15 +0200

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -13,6 +13,9 @@ object Main {
 
   /**
    * Exercise 1
+   * Do this exercise by implementing the pascal function in Main.scala, which takes a column c and a row r, 
+   * counting from 0 and returns the number at that spot in the triangle. For example, pascal(0,2)=1, pascal(1,2)=2 
+   * and pascal(1,3)=3.
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || c == r) 1
@@ -21,6 +24,8 @@ object Main {
 
   /**
    * Exercise 2
+   * Write a recursive function which verifies the balancing of parentheses in a string, 
+   * which we represent as a List[Char] not a String.
    */
 
   def balance(chars: List[Char]): Boolean = {
@@ -43,6 +48,9 @@ object Main {
 
   /**
    * Exercise 3
+   * Write a recursive function that counts how many different ways you can make change for an amount, 
+   * given a list of coin denominations. 
+   * For example, there are 3 ways to give change for 4 if you have coins with denomiation 1 and 2: 1+1+1+1, 1+1+2, 2+2.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money == 0) 1
